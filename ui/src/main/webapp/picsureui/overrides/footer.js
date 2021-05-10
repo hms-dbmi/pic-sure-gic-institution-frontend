479f6b8549f87b7e33f85dd401e87233f6b4eb83
Nathan Chu <chu.nathan@gmail.com>
2021-05-10 12:36:59 -0400

    import default overrides from baseline repo

---
+++ ui/src/main/webapp/picsureui/overrides/footer.js
@@ -0,0 +1,9 @@
+define(["handlebars"], function(HBS){
+	return {
+		/*
+		 * The render function for the footer can be overridden here.
+		 */
+		
+		render : undefined
+	};
+});
---
+++ ui/src/main/webapp/psamaui/overrides/footer.js
@@ -0,0 +1,8 @@
+define(["handlebars", "text!overrides/footer.hbs"], function(HBS, template){
+	return {
+		/*
+		 * The render function for the footer can be overridden here.
+		 */
+		render : undefined
+	};
+});
