301de6c7decfd81d501c71b113e2366fc67343a5
Jeremy Nix <Jeremy.Nix@cchmc.org>
2022-09-01 14:17:13 -0400

    add privilege to role

--- custom-migrations/auth/V8__SITE_ADMIN.sql
+++ custom-migrations/auth/V8__SITE_ADMIN.sql
@@ -1,8 +1,22 @@
 use auth;
 
- SET @uuidRole = REPLACE(UUID(),'-','');
-  INSERT INTO role VALUES (
-      unhex(@uuidRole),
-     'Data Admin',
-     'Allow access to Dataset Requests tab'
+SET @uuidRole = REPLACE(UUID(),'-','');
+INSERT INTO role VALUES (
+    unhex(@uuidRole),
+   'Data Admin',
+   'Allow access to Dataset Requests tab'
   );
+
+SET @uuidPrivilege = REPLACE(UUID(),'-','');
+INSERT INTO privilege (uuid, name, description, application_id)
+	VALUES (unhex(@uuidPrivilege),
+		'DATA_ADMIN',
+		'Data Admin privilege for PICSURE application',
+		(SELECT uuid FROM application WHERE name = 'PICSURE')
+	);
+
+INSERT INTO role_privilege (role_id, privilege_id)
+	VALUES (
+		unhex(@uuidRole),
+		unhex(@uuidPrivilege)
+	);
