12ce5d667eb4ea9cc31fec6b0393b22cf4db6b99
James <Jamestp19@gmail.com>
2023-05-26 12:27:21 -0400

    Override router instead of replacing core router

---
+++ ui/src/main/webapp/picsureui/overrides/router.js
@@ -0,0 +1,20 @@
+define([
+    'jquery',
+    'requestSearch/requestSearch',
+], function($, requestSearch){
+    const displayDatasetRequests = function () {
+        $('.header-btn.active').removeClass('active');
+        $('.header-btn[data-href='/picsureui/datasetRequests']').addClass('active');
+        $('#main-content').empty();
+        const requestSearchView = new requestSearch.View({
+          model: new requestSearch.Model(),
+        });
+        requestSearchView.render();
+        $('#main-content').append(requestSearchView.$el);
+      }
+	return {
+        routes : {
+            'picsureui/datasetRequests(/)': displayDatasetRequests,
+        }
+    };
+});
