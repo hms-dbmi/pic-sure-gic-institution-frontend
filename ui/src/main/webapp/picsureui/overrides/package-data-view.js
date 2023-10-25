629d374e3aab4362f1e10259398a3a6337e0427c
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2023-10-25 12:49:30 -0400

    Enable named dataset funcitonality and add genetic filters to view modal.

--- ui/src/main/webapp/picsureui/overrides/package-data-view.js
+++
@@ -1,9 +0,0 @@
-define(["jquery"], function($) {
-    return {
-        updateNamedDatasetObjects: function(self) {
-            $("#dataset-saved", self.$el).addClass('hidden');
-            $("#save-dataset-btn", self.$el).addClass('hidden');
-        },
-        saveDatasetId : function() {},
-    };
-});
