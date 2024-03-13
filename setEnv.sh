ea09f223ded1705db29ca20346c0842a28791da2
Luke Sikina <lucas.skina@gmail.com>
2024-03-13 12:48:01 -0400

    [ALS-5924] No custom paths inside docker

--- setEnv.sh
+++ setEnv.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 
-if [ ! $DOCKER_CONFIG_DIR ] ; then 
-    export DOCKER_CONFIG_DIR="/usr/local/docker-config" 
-fi 
-
-
-if ! cmp -s "$DOCKER_CONFIG_DIR/httpd/httpd-vhosts.conf" "ui/httpd-vhosts.conf"  ; then
-	mv $DOCKER_CONFIG_DIR/httpd/httpd-vhosts.conf $DOCKER_CONFIG_DIR/httpd/httpd-vhosts.conf.`date +%F_%T`
-	cp ui/httpd-vhosts.conf $DOCKER_CONFIG_DIR/httpd/httpd-vhosts.conf
+if ! cmp -s "/usr/local/docker-config/httpd/httpd-vhosts.conf" "ui/httpd-vhosts.conf"  ; then
+	mv /usr/local/docker-config/httpd/httpd-vhosts.conf /usr/local/docker-config/httpd/httpd-vhosts.conf.`date +%F_%T`
+	cp ui/httpd-vhosts.conf /usr/local/docker-config/httpd/httpd-vhosts.conf
 fi
 
 
